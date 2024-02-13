e8804c0546221ab2ff70f809487c601886541e56
Sanvika Kumar <sanvika890@gmail.com>
2024-02-13 18:32:13 +0530

    initial commit

--- index.js
+++ index.js
@@ -4,7 +4,7 @@
 // init project
 var express = require('express');
 var app = express();
-
+console.log("hello")
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
